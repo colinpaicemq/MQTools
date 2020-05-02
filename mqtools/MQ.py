dacf42bea672c683712c25dfd5cd5e90ce45e858
Colin Paice <colinpaicemq@gmail.com>
2020-05-02 10:14:15 +0100

    Test eclipse

--- mqtools/MQ.py
+++ mqtools/MQ.py
@@ -1,9 +1,12 @@
 import mqtools.smqpcf as SMQPCF
-from   . mqcode   import formatMQMD  as mqmd
-from   . mqcode   import format  as fo2 
+from   mqtools.mqcode   import formatMQMD  as mqmd
+from   mqtools.mqcode   import format  as fo2 
 #import mqtools.mqcode.fromMQMD as mqmd
 
 # from mqcode import formatMQMD 	  as mqmd
+def blah(aaa):
+    print("In blah")
+    pass
 def format_MQMD(md,strip="yes", debug=0):
     md = mqmd.format_MQMD(md,strip = strip,debug = debug)
     return md
